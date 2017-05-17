597cf3a5b491fd8c047b41fcd5744512f27bc20a
zhoude5 <zhoude5@live.cn>
2017-05-17 17:18:46 +0800

    多线程IO测试

---
+++ src/main/java/io/openmessaging/demo/TestMain.java
@@ -0,0 +1,70 @@
+package io.openmessaging.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by autulin on 2017/5/12.
+ */
+public class TestMain {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("./test");
+        Files.createDirectories(path);
+        Runnable runnable = () -> {
+            while (true) {
+                int t = getNewInt();
+                if (t > 100000) {
+                    System.out.println("end: "+System.currentTimeMillis()+ Thread.currentThread().getName());
+                    return;
+                }
+                try {
+                    Files.write(Paths.get("./test/"+t), new byte[]{(byte) i});
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        System.out.println("start: "+System.currentTimeMillis());
+        for (int i = 0; i < 8; i++) {
+            new Thread(runnable).start();
+        }
+
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        i = 0;
+        runnable = () -> {
+            byte[] bytes;
+            while (true) {
+                int t = getNewInt();
+                if (t > 100000) {
+                    System.out.println("end: "+System.currentTimeMillis()+ Thread.currentThread().getName());
+                    return;
+                }
+                try {
+                    bytes = Files.readAllBytes(Paths.get("./test/"+t));
+                    System.out.println(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        System.out.println("start: "+System.currentTimeMillis());
+        for (int i = 0; i < 8; i++) {
+            new Thread(runnable).start();
+        }
+
+    }
+
+    public static  int i = 0;
+    public static synchronized int getNewInt(){
+        return i++;
+    }
+
+}
